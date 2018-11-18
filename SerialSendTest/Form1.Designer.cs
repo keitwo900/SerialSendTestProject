09350cc6f8b710474f16cb47335534e151c1603d
kkasuga <kkasuga900@gmail.com>
2018-11-18 17:05:28 +0900

    ・シリアルポートデータ送信テストプロジェクト
    ■プロジェクト説明
    ・このプロジェクトは、C#でシリアルポート(COMポート)を選択、接続し、
    　文字列を送信するテストプロジェクトです。
    
    ・Visual Studio 2017での開発環境です。
    　個人の技術向上のために行っているので、Community Editionで開発しています。
    
    ■追加ファイル
    SerialSendTest.sln
    SerialSendTest/App.config
    SerialSendTest/Form1.Designer.cs
    SerialSendTest/Form1.cs
    SerialSendTest/Form1.resx
    SerialSendTest/Program.cs
    SerialSendTest/Properties/AssemblyInfo.cs
    SerialSendTest/Properties/Resources.Designer.cs
    SerialSendTest/Properties/Resources.resx
    SerialSendTest/Properties/Settings.Designer.cs
    SerialSendTest/Properties/Settings.settings
    SerialSendTest/SerialSendTest.csproj
    doc/commit_log_keitwo.txt

---
+++ SerialSendTest/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SerialSendTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox_Tx = new System.Windows.Forms.TextBox();
+			this.button_Tx = new System.Windows.Forms.Button();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.button_Connect = new System.Windows.Forms.Button();
+			this.comboBox_ComName = new System.Windows.Forms.ComboBox();
+			this.button_PortUpdate = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(24, 104);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(109, 19);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "送信文字列";
+			// 
+			// textBox_Tx
+			// 
+			this.textBox_Tx.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox_Tx.Location = new System.Drawing.Point(16, 136);
+			this.textBox_Tx.Name = "textBox_Tx";
+			this.textBox_Tx.Size = new System.Drawing.Size(296, 26);
+			this.textBox_Tx.TabIndex = 1;
+			// 
+			// button_Tx
+			// 
+			this.button_Tx.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button_Tx.Location = new System.Drawing.Point(328, 136);
+			this.button_Tx.Name = "button_Tx";
+			this.button_Tx.Size = new System.Drawing.Size(96, 32);
+			this.button_Tx.TabIndex = 2;
+			this.button_Tx.Text = "送信";
+			this.button_Tx.UseVisualStyleBackColor = true;
+			this.button_Tx.Click += new System.EventHandler(this.button_Tx_Click);
+			// 
+			// serialPort1
+			// 
+			this.serialPort1.BaudRate = 115200;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label2.Location = new System.Drawing.Point(24, 8);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(233, 19);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Port番号を選択してください";
+			// 
+			// button_Connect
+			// 
+			this.button_Connect.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button_Connect.Location = new System.Drawing.Point(360, 32);
+			this.button_Connect.Name = "button_Connect";
+			this.button_Connect.Size = new System.Drawing.Size(96, 32);
+			this.button_Connect.TabIndex = 2;
+			this.button_Connect.Text = "接続";
+			this.button_Connect.UseVisualStyleBackColor = true;
+			this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+			// 
+			// comboBox_ComName
+			// 
+			this.comboBox_ComName.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.comboBox_ComName.FormattingEnabled = true;
+			this.comboBox_ComName.Location = new System.Drawing.Point(128, 32);
+			this.comboBox_ComName.Name = "comboBox_ComName";
+			this.comboBox_ComName.Size = new System.Drawing.Size(121, 27);
+			this.comboBox_ComName.TabIndex = 3;
+			// 
+			// button_PortUpdate
+			// 
+			this.button_PortUpdate.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.button_PortUpdate.Location = new System.Drawing.Point(256, 32);
+			this.button_PortUpdate.Name = "button_PortUpdate";
+			this.button_PortUpdate.Size = new System.Drawing.Size(96, 32);
+			this.button_PortUpdate.TabIndex = 2;
+			this.button_PortUpdate.Text = "更新";
+			this.button_PortUpdate.UseVisualStyleBackColor = true;
+			this.button_PortUpdate.Click += new System.EventHandler(this.button_PortUpdate_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.comboBox_ComName);
+			this.Controls.Add(this.button_PortUpdate);
+			this.Controls.Add(this.button_Connect);
+			this.Controls.Add(this.button_Tx);
+			this.Controls.Add(this.textBox_Tx);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Tx;
+        private System.Windows.Forms.Button button_Tx;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.ComboBox comboBox_ComName;
+        private System.Windows.Forms.Button button_PortUpdate;
+    }
+}
+
---
+++ SerialSendTest/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialSendTest
+{
+	public partial class Form1 : Form
+	{
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// シリアルポートを取得して、ComboBoxのリストに追加します。
+		/// </summary>
+		void GetComName()
+		{
+			string[] ports = SerialPort.GetPortNames(); //ポート取得
+
+			//ComboBoxに追加
+			foreach (string port in ports)
+			{
+				comboBox_ComName.Items.Add(port);
+			}
+		}
+
+		//-------------------------------------------------------------------
+		//-------------------------------------------------------------------
+		// 以下、フォームのイベント関連
+		//-------------------------------------------------------------------
+		//-------------------------------------------------------------------
+
+		/// <summary>
+		/// ポート名更新ボタンが押されたとき
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button_PortUpdate_Click(object sender, EventArgs e)
+		{
+			GetComName();
+		}
+
+		/// <summary>
+		/// 接続ボタンが押されたとき
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button_Connect_Click(object sender, EventArgs e)
+		{
+			if (button_Connect.Text == "接続") //ポートオープン
+			{
+				if (comboBox_ComName.Text == "")
+				{
+					MessageBox.Show("ポートを選択してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					return;
+				}
+				serialPort1.PortName = comboBox_ComName.Text;
+
+				try
+				{
+					serialPort1.Open();
+					button_Connect.Text = "切断";
+				}
+				catch (System.UnauthorizedAccessException ex) //別プロセスがすでに指定したCOMを開いている場合など
+				{
+					MessageBox.Show(ex.ToString());
+				}
+			}
+			else //ポートクローズ
+			{
+				serialPort1.Close();
+				button_Connect.Text = "接続";
+			}
+		}
+
+		/// <summary>
+		/// 送信ボタンが押されたとき
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button_Tx_Click(object sender, EventArgs e)
+		{
+			if (serialPort1.IsOpen)
+			{
+				serialPort1.Write(textBox_Tx.Text + "\r\n");
+			}
+			else
+			{
+				MessageBox.Show("シリアルポートは閉じられています。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		/// <summary>
+		/// フォームロード
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			GetComName();
+
+			//COMポートが1つでも存在した場合は、最初に検出したCOMポートをデフォルトでセットする
+			if(comboBox_ComName.Items.Count != 0)
+			{
+				comboBox_ComName.Text = comboBox_ComName.Items[0].ToString();
+			}
+		}
+	}
+}
